34084c1e57f5152d7be1b0d09b63979e8c8fc18f
kpan13 <kajaljpan@gmail.com>
2024-09-03 16:34:11 -0700

    step 1

---
+++ index.py
@@ -0,0 +1,7 @@
+#Step 1: Understanding Magic Methods
+#Magic methods are a special piece of Python programming. They are specific to Object Oriented 
+# Programming in Python, meaning that they can only be implemented in, and will only work in, classes.
+#They allow actions to be performed on class instance objects that are otherwise impossible, such as printing
+#  and comparing.Magic methods will always take the __<>__() form. For example, in the following code snippet
+# the __str__() magic method is defined for the Car class.
+
